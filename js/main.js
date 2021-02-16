de389281b2c2d9c76972e98907f3f8e8d6c610f3
milenkodev <milenko.ilic18@gmail.com>
2021-02-16 13:28:28 +0100

    Working on bar

--- js/main.js
+++ js/main.js
@@ -16,31 +16,31 @@ gradientStroke.addColorStop(0, '#EA6060');
 
 
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'doughnut',
+  // The type of chart we want to create
+  type: 'doughnut',
 
 
 
-    
-    // The data for our dataset
-    data: {
-        
-        datasets: [{
-            backgroundColor: [ gradientStroke, '#939393'],
-            borderColor: '#787878',
-            borderWidth: '3',
-            data: [100, 20],
-            
-        }]
-    },
 
-    // Configuration options go here
-    options: {
-      responsive: true,
-          	rotation: 1 * Math.PI,
-            circumference: 1 * Math.PI
+  // The data for our dataset
+  data: {
 
-    }
+    datasets: [{
+      backgroundColor: [gradientStroke, '#939393'],
+      borderColor: '#787878',
+      borderWidth: '3',
+      data: [100, 20],
+
+    }]
+  },
+
+  // Configuration options go here
+  options: {
+    responsive: true,
+    rotation: 1 * Math.PI,
+    circumference: 1 * Math.PI
+
+  }
 });
 
 
@@ -48,87 +48,95 @@ var chart = new Chart(ctx, {
 
 // Accerleration 
 
-  var ctxAcc = document.getElementById("chartAcceleration").getContext("2d");
-  
-  var myChart = new Chart(ctxAcc, {
-    type: 'line',
-   
-    data: {
-    
-      labels: ["0", "1", "2", "3", "4", "5"],
-      datasets: [{
+var ctxAcc = document.getElementById("chartAcceleration").getContext("2d");
 
-        borderColor: '#F58A8A',
-        backgroundColor: '#F58A8A',
-        borderWidth: 4,
-        
+var myChart = new Chart(ctxAcc, {
+  type: 'line',
 
-        steppedLine: true,
-        fill: false,
-        
-        data: [-10, 30, 50, 90, 30, 10]
+  data: {
+
+    labels: ["0", "1", "2", "3", "4", "5"],
+    datasets: [{
+
+      borderColor: '#F58A8A',
+      backgroundColor: '#F58A8A',
+      borderWidth: 4,
+
+
+      steppedLine: true,
+      fill: false,
+
+      data: [-10, 30, 50, 90, 30, 10]
+    }]
+  },
+  options: {
+
+    responsive: true,
+    tooltips: {
+      mode: 'index',
+      intersect: true
+    },
+
+
+    fullWidth: true,
+    annotation: {
+      annotations: [{
+        type: 'line',
+        mode: 'horizontal',
       }]
     },
-    options: {
 
-        responsive: true,
-        tooltips: {
-          mode: 'index',
-          intersect: true
+
+
+    // Custome gridlines
+
+
+    // customize Axes lines 
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          fontColor: "#fff", // this here
+
         },
 
-        
-        fullWidth: true,
-        annotation: {
-          annotations: [{
-            type: 'line',
-            mode: 'horizontal',
-          }]
+        gridLines: {
+          display: true,
+          zeroLineColor: '#fff',
+          zeroLineWidth: 2,
+
+        }
+      }],
+      yAxes: [{
+        display: true,
+        gridLines: {
+          display: true,
+        },
+        ticks: {
+          fontColor: "#fff", // this here
         },
 
+        gridLines: {
+          display: true,
+          zeroLineColor: '#fff',
+          zeroLineWidth: 2,
 
+        }
+      }],
+    }
 
-        // Custome gridlines
-         
-
-// customize Axes lines 
-        scales: {
-          xAxes: [{ 
-              gridLines: {
-                  display: false,
-              },
-              ticks: {
-                fontColor: "#fff", // this here
-                
-              },
-
-              gridLines: {
-                display: true,
-                zeroLineColor: '#fff',
-                zeroLineWidth: 2,
-  
-              }
-          }],
-          yAxes: [{
-              display: true,
-              gridLines: {
-                  display: true,
-              },
-              ticks: {
-                fontColor: "#fff", // this here
-              },
-
-              gridLines: {
-                display: true,
-                zeroLineColor: '#fff',
-                zeroLineWidth: 2,
-               
-              }
-          }],
-      }
-      
-      }
-  });
+  }
+});
 
 
 
+//  Fuel bar
+var ctxFuel = document.getElementById('chartFuel').getContext('2d');
+
+var myBarChart = new Chart(ctxFuel, {
+  type: 'bar',
+  data: data,
+  options: options
+});
