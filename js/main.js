71a7e24df705d19770e5e780a73acc5beae4bcae
Miloschool <19077165@student.hhs.nl>
2021-02-25 09:29:59 +0100

    Cleaning up

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-// Fuel
+// Speed
 var ctx = document.getElementById('chartSpeed').getContext('2d');
-
 Chart.defaults.global.legend.display = false;
 
 
 
-
+// Speed colors
 var gradientStroke = ctx.createLinearGradient(200, 0, 50, 20);
 gradientStroke.addColorStop(1, '#45D720');
-
 gradientStroke.addColorStop(0.5, '#F3A939');
 gradientStroke.addColorStop(0, '#EA6060');
 
@@ -16,30 +14,22 @@ gradientStroke.addColorStop(0, '#EA6060');
 
 
 var chart = new Chart(ctx, {
-  // The type of chart we want to create
   type: 'doughnut',
 
-
- 
-  // The data for our dataset
   data: {
-
     datasets: [{
       label: 'Fuel',
       backgroundColor: [gradientStroke, '#939393'],
       borderColor: '#787878',
       borderWidth: '3',
       data: [100, 20],
-
     }]
   },
-
   // Configuration options go here
   options: {
     responsive: true,
     rotation: 1 * Math.PI,
     circumference: 1 * Math.PI
-
   }
 });
 
@@ -47,49 +37,35 @@ var chart = new Chart(ctx, {
 
 
 // Accerleration 
-
 var ctxAcc = document.getElementById("chartAcceleration").getContext("2d");
-
 var myChart = new Chart(ctxAcc, {
   type: 'line',
 
   data: {
-
     labels: ["0", "1", "2", "3", "4", "5"],
     datasets: [{
-
       borderColor: '#F58A8A',
       backgroundColor: '#F58A8A',
       borderWidth: 4,
-
-
       steppedLine: true,
       fill: false,
-
       data: [-10, 30, 50, 90, 30, 10]
     }]
   },
-  options: {
 
+  options: {
+    fullWidth: true,
     responsive: true,
     tooltips: {
       mode: 'index',
       intersect: true
     },
-
-
-    fullWidth: true,
     annotation: {
       annotations: [{
         type: 'line',
         mode: 'horizontal',
       }]
     },
-
-
-
-    // Custome gridlines
-
 
     // customize Axes lines 
     scales: {
@@ -98,7 +74,7 @@ var myChart = new Chart(ctxAcc, {
           display: false,
         },
         ticks: {
-          fontColor: "#fff", // this here
+          fontColor: "#fff", // color change for ticks
 
         },
 
@@ -106,7 +82,6 @@ var myChart = new Chart(ctxAcc, {
           display: true,
           zeroLineColor: '#fff',
           zeroLineWidth: 2,
-
         }
       }],
       yAxes: [{
@@ -117,16 +92,13 @@ var myChart = new Chart(ctxAcc, {
         ticks: {
           fontColor: "#fff", // this here
         },
-
         gridLines: {
           display: true,
           zeroLineColor: '#fff',
           zeroLineWidth: 2,
-
         }
       }],
     }
-
   }
 });
 
@@ -134,7 +106,6 @@ var myChart = new Chart(ctxAcc, {
 // Fuel
 
 var ticks = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]; // custom ticks xAxes
-//  Fuel bar
 var ctxFuel = document.getElementById('chartFuel').getContext('2d');
 
 var myBarChart = new Chart(ctxFuel, {
@@ -147,13 +118,11 @@ var myBarChart = new Chart(ctxFuel, {
     }]
   },
 
-
   options: {
-    //Customizing xAxes
     scales: {
       xAxes: [{
         ticks: {
-          fontColor: "#fff", // this here
+          fontColor: "#fff", 
           autoSkip: false,
           min: ticks[ticks.length - 1],
           max: ticks[0]
@@ -169,11 +138,9 @@ var myBarChart = new Chart(ctxFuel, {
           display: true,
           zeroLineColor: '#3F4347',
           zeroLineWidth: 2,
-
         }
       }]
     }
-    //END customizing xAxes
   }
 });
 
@@ -187,30 +154,22 @@ var myChartDistance = new Chart(ctxDist, {
   type: 'line',
 
   data: {
-
     labels: ["0", "1", "2", "3", "4", "5", "6"],
     datasets: [{
-
       borderColor: '#58E5C4',
       backgroundColor: '#58E5C4',
       borderWidth: 4,
-
-
-  
       fill: false,
-
       data: [0, 5, 15, 32, 52, 74, 98]
     }]
   },
-  options: {
 
+  options: {
     responsive: true,
     tooltips: {
       mode: 'index',
       intersect: true
     },
-
-
     fullWidth: true,
     annotation: {
       annotations: [{
@@ -218,39 +177,30 @@ var myChartDistance = new Chart(ctxDist, {
         mode: 'horizontal',
       }]
     },
-
-
-
-    // Custome gridlines
-
-
-    // customize Axes lines 
     scales: {
       xAxes: [{
         gridLines: {
           display: false,
         },
         ticks: {
-          fontColor: "#fff", // this here
-
+          fontColor: "#fff", 
         },
 
         gridLines: {
           display: true,
           zeroLineColor: '#fff',
           zeroLineWidth: 2,
-
         }
       }],
+
       yAxes: [{
         display: true,
         gridLines: {
           display: true,
         },
         ticks: {
-          fontColor: "#fff", // this here
+          fontColor: "#fff",
         },
-
         gridLines: {
           display: true,
           zeroLineColor: '#fff',
